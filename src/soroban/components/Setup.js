ce548128dd06a17ca5dd55aec21fbe5e8193adbf
santosh214 <santoshsingh74827@gmail.com>
2023-03-22 23:51:03 +0530

    add

--- src/soroban/components/Setup.js
+++ src/soroban/components/Setup.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
 export default function Setup() {
-  return <div>Setup soroban</div>;
+  return (
+
+    <>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>Setup rust</h1>
+        </div>
+      </div>
+    </div>
+    </>
+  );
 }
